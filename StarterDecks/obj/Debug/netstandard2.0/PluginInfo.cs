38fb5722bbd1a73a3ce57ba0f5af8be98eb5770f
divisionbyz0rro <divisionbyzorro@gmail.com>
2021-11-19 08:48:37 -0600

    Delete unnecessary files

--- StarterDecks/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace StarterDecks
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "Infiniscryption.StarterDecks";
-        public const string PLUGIN_NAME = "Infiniscryption.StarterDecks";
-        public const string PLUGIN_VERSION = "1.0";
-    }
-}
